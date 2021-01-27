777dcc414d0980a892acd7a23b1394cdc7c2b964
Juan Ensuncho <jsensunchop@unal.edu.co>
2021-01-27 16:37:43 -0500

    ViewModel implementation

--- app/src/main/java/com/example/dogs/view/ListFragment.kt
+++ app/src/main/java/com/example/dogs/view/ListFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dogs.R
+import com.example.dogs.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
+    private val dogListAdapter = DogListAdapter(arrayListOf())
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,6 +43,49 @@ class ListFragment : Fragment() {
             Navigation.findNavController(it).navigate(action)
         }
          */
-    }
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java) //viewmodel instanciado en el list fragment
+        viewModel.refresh() // actualizar lo que hay en el listviewmodel
 
+        //ahora se instanciara la dog list (recycle view)
+
+        dogsList.apply {
+            layoutManager = LinearLayoutManager(context)//esto le permite al sistema ordenar los elementos sequencialmente en forma de lista
+            //attach the adapter
+            adapter = dogListAdapter
+        }
+        observeViewModel()
+    }
+     //esta funcion sirve para usar las variables que se crearon en el ListViewModel para actualizar el layout basado en los valores que se tomen
+   fun observeViewModel(){
+         viewModel.dogs.observe(this, Observer { dogs ->
+             dogs?.let { //verificacion de nulidad
+                 dogsList.visibility = View.VISIBLE
+                 dogListAdapter.updateDogList(dogs)
+             }
+         })
+
+         //En la vista, si existe algun eror esto lo muestra
+         viewModel.dogsLoadError.observe(this, Observer { isError ->
+             isError?.let {
+                 listError.visibility = View.VISIBLE
+             }
+         })
+
+         // en la vista si se esta cargando, muestra el spinner, sino desaparece
+         viewModel.dogsLoadError.observe(this, Observer { isLoading ->
+             isLoading?.let {
+                 loadingView.visibility = if(it) {
+                     View.VISIBLE //si esta cargando, mostrar el spinner
+                 } else {
+                     View.GONE // no mostrar
+                 }
+                 //mostrar las otras vistas dependiendo del spinner
+                 if (it) {
+                     listError.visibility = View.GONE //ocultar vista (solo mostrar spinner)
+                     dogsList.visibility = View.GONE //ocultar vista (solo mostrar spinner)
+                 }
+             }
+
+         })
+     }
 }
--- app/src/main/java/com/example/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/dogs/viewmodel/ListViewModel.kt
@@ -1,4 +1,32 @@
 package com.example.dogs.viewmodel
 
-class ListViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.dogs.model.DogBreed
+
+class ListViewModel : ViewModel() {
+
+    //variables tipo livedata
+
+    //va a proveer la informacion para la lista actual de dogs que se va a traer del origen de la data
+    val dogs = MutableLiveData<List<DogBreed>>()
+    //notificara si esta escuchando al view model y notificara si hay algun error pero no sera especifico
+    //especificara si hay algun error al adquirir la data
+    val dogsLoadError = MutableLiveData<Boolean>()
+    // le dira al componente que este escuchando que el sistema se esta cargando, entonces la data no ha llegado pero no hay ningun error
+    //mostrara un spinner al usuario
+    val loading = MutableLiveData<Boolean>()
+
+    //TBD
+    fun refresh(){
+        val dog1 = DogBreed("1", "Corgi", "15 years", "breedGroup", "bredFor", "temperament", "")
+        val dog2 = DogBreed("2", "Labrador", "15 years", "breedGroup", "bredFor", "temperament", "")
+        val dog3 = DogBreed("3", "Rotwailer", "15 years", "breedGroup", "bredFor", "temperament", "")
+        val dogList = arrayListOf<DogBreed>(dog1, dog2, dog3)
+
+        dogs.value = dogList
+        dogsLoadError.value = false
+        loading.value = false
+    }
+
 }
