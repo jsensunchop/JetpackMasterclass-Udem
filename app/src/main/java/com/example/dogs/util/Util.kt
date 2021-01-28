7c00cee116a0abed0c7f2977e3ad6ca3ba4a181e
Juan Ensuncho <jsensunchop@unal.edu.co>
2021-01-28 16:48:41 -0500

    using Glide-1

--- app/src/main/java/com/example/dogs/util/Util.kt
+++ app/src/main/java/com/example/dogs/util/Util.kt
@@ -1,2 +1,12 @@
 package com.example.dogs.util
 
+import android.content.Context
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+
+fun getProgressDrawable(context: Context): CircularProgressDrawable{
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
